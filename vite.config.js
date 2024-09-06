5610679b4e84c628abe53c7308216ff55d36f0d5
kaushal1717 <kpadaliya1717@gmail.com>
2024-09-06 09:17:34 +0530

    undo updates

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
